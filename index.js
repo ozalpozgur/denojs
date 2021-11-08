9ca886a2f309a4e85a14d7d75dee54f7cb054aa9
Özgür ÖZALP <91522401+ozalpozgur@users.noreply.github.com>
2021-11-08 17:09:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+
+async function handler(req) {
+	const res = await fetch('https://www.turkishairlines.com/com.thy.web.online.ibs/ibs/booking/location/suggest', {
+		headers: {
+			'accept-language': 'tr',
+			country: 'tr',
+			page: 'https://www.turkishairlines.com/tr-tr/',
+		},
+	});
+  return res.json();
+}
+
+console.log("Listening on http://localhost:8000");
+await listenAndServe(":8000", handler);
